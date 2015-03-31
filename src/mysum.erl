f99dea6783a5ca83d980e8f003a36237edfda4cd
sparrell <duncan@sfractal.com>
2015-03-31 08:15:28 -0400

    wrote some mysum tests

--- src/mysum.erl
+++ src/mysum.erl
@@ -55,5 +55,11 @@ reverse_nil_test() -> [] = lists:reverse([]).
 reverse_one_test() -> [1] = lists:reverse([1]).
 reverse_two_test() -> [2,1] = lists:reverse([1,2]).
 
+sum_0_test() -> 0 = sum(0).
+sum_1_test() -> 1 = sum(1).
+sum_2_test() -> 3 = sum(2).
+sum_3_test() -> 6 = sum(3).
+sum_4_test() -> 10 = sum(4).
+
 -endif.
 
