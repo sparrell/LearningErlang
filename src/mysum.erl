8aca08c703371d1f6d04b3aae591cda978f01793
sparrell <duncan@sfractal.com>
2015-03-31 08:27:00 -0400

    comment tests written

--- src/mysum.erl
+++ src/mysum.erl
@@ -51,15 +51,18 @@ even_printAll( [_ | Rest ] ) ->
 
 -ifdef(TEST).
 
+% tests of erlang to make sure tests running (did these first)
 reverse_nil_test() -> [] = lists:reverse([]).
 reverse_one_test() -> [1] = lists:reverse([1]).
 reverse_two_test() -> [2,1] = lists:reverse([1,2]).
 
+%simple tests of sum function
 sum_0_test() -> 0 = sum(0).
 sum_1_test() -> 1 = sum(1).
 sum_2_test() -> 3 = sum(2).
 sum_3_test() -> 6 = sum(3).
 sum_4_test() -> 10 = sum(4).
+%learn how to do negative tests (ie catch expected exceptions)
 sum_neg_test() -> ?assertError(function_clause, sum(-1)).
 
 -endif.
